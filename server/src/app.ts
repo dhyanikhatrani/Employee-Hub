4593ebc6ba78c34e7c40982ae13fd8b84105dc78
Dhyani Khatrani <dhyanikhatrani41@gmail.com>
2026-07-15 16:06:33 +0530

    "feat: setup MongoDB database connection"

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "EmployeeHub HRMS Backend Running 🚀",
+  });
+});
+
+export default app;
