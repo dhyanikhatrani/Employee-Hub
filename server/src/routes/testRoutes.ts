22dfe0024db03eeb25a9252a8c79492170b79d5e
Dhyani Khatrani <dhyanikhatrani41@gmail.com>
2026-07-16 19:58:45 +0530

    "feat: implement JWT authorization middleware"

--- server/src/routes/testRoutes.ts
+++ server/src/routes/testRoutes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware";
+import { authorize } from "../middleware/roleMiddleware";
 
 const router = Router();
 
-router.get("/profile", protect, (req, res) => {
-  res.json({
-    success: true,
-    message: "Protected Route Accessed Successfully",
-  });
-});
+router.get(
+  "/profile",
+  protect,
+  authorize("hr", "admin"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome HR/Admin",
+    });
+  }
+);
 
 export default router;
