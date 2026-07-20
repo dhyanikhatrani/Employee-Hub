3c126c42f22672c68f86492972d9874f25594bc7
Dhyani Khatrani <dhyanikhatrani41@gmail.com>
2026-07-20 17:17:53 +0530

    "feat: complete attendance CRUD module"

--- server/src/controllers/attendanceController.ts
+++ server/src/controllers/attendanceController.ts
@@ -96,3 +96,110 @@ res.status(200).json({
     });
   }
 };
+//get attendance by employee ID
+export const getAttendanceById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const attendance = await Attendance.findById(id).populate({
+      path: "employeeId",
+      populate: {
+        path: "userId",
+        select: "fullName email",
+      },
+    });
+
+    if (!attendance) {
+      res.status(404).json({
+        success: false,
+        message: "Attendance not found",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: attendance,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+//update attendance 
+export const updateAttendance = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const updatedAttendance = await Attendance.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedAttendance) {
+      res.status(404).json({
+        success: false,
+        message: "Attendance not found",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Attendance updated successfully",
+      data: updatedAttendance,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+//delete attendance 
+export const deleteAttendance = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const attendance = await Attendance.findById(id);
+
+    if (!attendance) {
+      res.status(404).json({
+        success: false,
+        message: "Attendance not found",
+      });
+      return;
+    }
+
+    await Attendance.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Attendance deleted successfully",
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
--- server/src/routes/attendanceRoutes.ts
+++ server/src/routes/attendanceRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAttendance, getAllAttendance, } from "../controllers/attendanceController";
+import { createAttendance, getAllAttendance,  getAttendanceById,updateAttendance,deleteAttendance,} from "../controllers/attendanceController";
 import { protect } from "../middleware/authMiddleware";
 import { authorize } from "../middleware/roleMiddleware";
 
@@ -16,6 +16,24 @@ router.get(
   protect,
   authorize("admin", "hr"),
   getAllAttendance
+);
+router.get(
+  "/:id",
+  protect,
+  authorize("admin", "hr"),
+  getAttendanceById
+);
+router.put(
+  "/:id",
+  protect,
+  authorize("admin", "hr"),
+  updateAttendance
+);
+router.delete(
+  "/:id",
+  protect,
+  authorize("admin", "hr"),
+  deleteAttendance
 );
 
 export default router;
